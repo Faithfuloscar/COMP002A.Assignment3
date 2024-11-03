c5642c03b35ae49de5045d9ce9567d4125bd2f1a
Faithfuloscar <oscarmendez2003@outlook.com>
2024-11-03 12:25:34 -0800

    created a function to find the circumference for a diameter of value of 10

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function greet(name) {
     return `Hello, ${name}`;
 }
 console.log(greet("some name"));
+
+// Circumference Function
+function circumference(diameter) {
+    const pi = Math.PI;
+    return pi * diameter;
+}
+
+console.log(circumference(10));
