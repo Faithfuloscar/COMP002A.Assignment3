b2eaaa5dfbc0875542ab0539bffb055c707c8116
Faithfuloscar <oscarmendez2003@outlook.com>
2024-11-03 12:20:54 -0800

    completed greeting function

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Author: Oscar M
+Purpose: To write functions that outputs a greeting, approximates circumference of a circle, and one that calls another function
---
+++ script.js
@@ -0,0 +1,6 @@
+// Greeting function
+function greet(name) {
+    // ` is used for string interpolation 
+    return `Hello, ${name}`;
+}
+console.log(greet("some name"));
